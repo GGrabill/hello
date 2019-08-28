cb7f40316d72e7e61427b0c123246628013962b4
Gavin <gavin.grabill@huskers.unl.edu>
2019-08-28 14:22:08 -0500

    Initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Gavin Grabill
+ * Date: 2019/08/28
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stcdio.h>
+
+int main(int argc, char **argv) {
+
+   printf("Gavin Grabill\n");
+
+   return 0;
+}
